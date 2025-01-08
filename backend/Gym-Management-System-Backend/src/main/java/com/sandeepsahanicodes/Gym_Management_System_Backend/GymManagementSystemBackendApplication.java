c69e1b47bf8bd615eb3230acdc4b2bad52992776
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-01-08 22:49:40 +0530

    Added SpringBoot initializr

---
+++ backend/Gym-Management-System-Backend/src/main/java/com/sandeepsahanicodes/Gym_Management_System_Backend/GymManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sandeepsahanicodes.Gym_Management_System_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ backend/Gym-Management-System-Backend/src/test/java/com/sandeepsahanicodes/Gym_Management_System_Backend/GymManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeepsahanicodes.Gym_Management_System_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
