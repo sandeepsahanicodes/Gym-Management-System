0772665aeaebd27b39e800547f6a3e3644914493
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-01-27 16:21:27 +0530

    Xcode project initialzation

---
+++ mobile-frontend/mobile-frontend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile-frontend
+//
+//  Created by Sandeep Sahani on 27/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
