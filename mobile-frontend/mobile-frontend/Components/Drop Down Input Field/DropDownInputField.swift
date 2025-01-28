05958af6e1e415f6d7854e6cf7c5f2dc534d94d0
Sandeep Sahani <sandeepsahanigithub@gmail.com>
2025-01-28 15:27:22 +0530

    Created custom input field

---
+++ mobile-frontend/mobile-frontend/Components/Drop Down Input Field/DropDownInputField.swift
@@ -0,0 +1,12 @@
+//
+//  DropDownInputField.swift
+//  mobile-frontend
+//
+//  Created by Sandeep Sahani on 28/01/25.
+//
+
+import UIKit
+
+class DropDownInputField: InputField {
+    
+}
---
+++ mobile-frontend/mobile-frontend/Components/Input Field/InputField.swift
@@ -0,0 +1,49 @@
+//
+//  InputField.swift
+//  mobile-frontend
+//
+//  Created by Sandeep Sahani on 27/01/25.
+//
+
+import UIKit
+
+public class InputField: UIView {
+
+    @IBOutlet private var containerView: UIView!
+    @IBOutlet private weak var inputFieldHeading: UILabel!
+    @IBOutlet private weak var inputTextField: UITextField!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commitInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commitInit()
+    }
+    
+    private func commitInit() {
+        Bundle.main.loadNibNamed("InputField", owner: self, options: nil)
+        addSubview(containerView)
+        containerView.frame = self.bounds
+        containerView.autoresizingMask = [.flexibleHeight,.flexibleWidth]
+    }
+     
+    /// Configures the input field.
+    /// - Parameters:
+    ///   - heading: The heading or title for the input field.
+    ///   - placeholder: The placeholder text displayed in the input field.
+    ///   - contentType: The type of content expected in the input field, used for keyboard optimization and autofill.
+    public func configure(heading: String, placeholder: String, contentType: UITextContentType) {
+        self.inputFieldHeading.text = heading
+        self.inputTextField.placeholder = placeholder
+        self.inputTextField.textContentType = contentType
+        
+        if contentType == .password {
+            self.inputTextField.isSecureTextEntry = true
+        } else {
+            self.inputTextField.isSecureTextEntry = false
+        }
+    }
+}
